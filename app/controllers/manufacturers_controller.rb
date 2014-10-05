1a33430698df922c2abb0130ccb8262085ce694a
louis hoang <bawsing@gmail.com>
2014-10-05 13:33:54 -0400

    added view for individual manufacturer user can add new manufacturer

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -2,4 +2,28 @@ class ManufacturersController < ApplicationController
   def index
     @manufacturers = Manufacturer.all
   end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+
+    if @manufacturer.save
+      redirect_to manufacturers_path, notice: "Your record has been created successfully!"
+    else
+      flash[:notice] = "Your record could not be completed"
+      render "new"
+    end
+
+  end
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :country)
+  end
 end
