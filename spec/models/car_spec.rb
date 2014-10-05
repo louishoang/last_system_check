c970f3f5dbfab500b0282b377a9745056cf86d20
louis hoang <bawsing@gmail.com>
2014-10-05 16:56:48 -0400

    added more validate for car unit test

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe Car do
   describe 'validations' do
     it { should validate_presence_of(:color) }
+    it { should validate_numericality_of(:year) }
+    it { should validate_numericality_of(:mileage) }
     it { should validate_presence_of(:manufacturer) }
   end
 
